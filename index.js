0664d828bb45df2582e6fe649b700d7efb9aeee5
Chaitanya Mhatre <cmh24292@gmail.com>
2020-12-09 22:30:01 +0530

    Adding HTML elements and CSS classes Via JS

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-// toggle function
+//create elements
+function createImageElement(){
+ var node = document.createElement("IMG");
+	 node.src = "img/temple.jpg";
+     node.classList.add("card-size");
+	 
+ return node;
+}
 
+function createMapElement(){
+var node = document.createElement("IFRAME");
+	node.src = "https://maps.google.com/maps?q=18.786881%2C%2072.885363&t=&z=13&ie=UTF8&iwloc=&output=embed";
+	node.classList.add("card-size");
+
+return node;
+}
+
+function createH2Element(){	
+var node  =  document.createElement("H2");
+var	nodeData = document.createTextNode("Goteshwar Mandir Map");
+	node.appendChild(nodeData);
+	
+return node;
+}
+// toggle function
 function toggleElement(){
  var checkBox = document.getElementById("toggleId");
- var mapData = '<iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=18.786881%2C%2072.885363&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe> <h2>Goteshwar Mandir Map</h2>';
- var imgData ='<img src="img/temple.jpg" style="width:600px;height:500px;" />';
  var mapDiv = document.getElementById("mapDiv");
  var imgDiv = document.getElementById("imgDiv");
  
  if (checkBox.checked == true){
 	 imgDiv.innerHTML="";
-	 mapDiv.innerHTML = mapData; 
+	 var mapElement = createMapElement();
+	 var H2Element = createH2Element();
+	 mapDiv.appendChild(mapElement);
+	 mapDiv.appendChild(H2Element);
  }else{
 	 mapDiv.innerHTML ="";
-	 imgDiv.innerHTML = imgData;
+	 imgDiv.appendChild(createImageElement());
  }
  
 }
-
+//on load image
 function imgLoad(){
- var imgData ='<img src="img/temple.jpg" style="width:600px;height:500px;" /> <h2>Goteshwar Mandir</h2>';
  var imgDiv = document.getElementById("imgDiv");
-	 imgDiv.innerHTML = imgData;
+	 imgDiv.appendChild(createImageElement());
 }
